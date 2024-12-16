f0e34103714d16edc57baddd0277befb8b06c87a
John Vimrish <john.vimri@gmail.com>
2024-12-16 13:09:11 -0800

    latest code changes

--- main.py
+++ main.py
@@ -5,34 +5,28 @@ from model import GPTLanguageModel
  
 
 class static_variables() :
-
-
     BATCH_SIZE = 16 
-    BLOCK_SIZE = 32 
-    MAX_ITERS = 10000
-    EVAL_INTERVAL = 1000
-    LEARNING_RATE = 1E-3
+    BLOCK_SIZE = 8 
+    MAX_ITERS = 100
+    EVAL_INTERVAL = 500
+    LEARNING_RATE = 0.001
     EVAL_ITERS = 200
     RANDOM_SEED = 1337
-    WORD_COUNT = 10000
-    n_embd =  64
+    WORD_COUNT = 100
+    n_embd =  32
     n_head =  4
-    n_layer =  4
+    n_layer =  3
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     dropout = 0.2
+    DATA_FILE = "input.txt"
 
-
-#this is main part 
 
 def main():
-
-    DATA_FILE = "input.txt"
     torch.manual_seed(static_variables.RANDOM_SEED)
-    data = Data(DATA_FILE)
+    data = Data(static_variables.DATA_FILE)
     model = GPTLanguageModel(data.vocab_size,static_variables)
     trainer = Trainer(data.get_data(), model,static_variables)
     context =trainer.train(static_variables.MAX_ITERS)
-    # print(context)
     generated = model.generate(context, static_variables.WORD_COUNT)[0].tolist()
     print(data.decode(generated))
 
